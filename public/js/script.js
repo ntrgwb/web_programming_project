46a0d47eb5d75949229fa7e8f2ba4091323b0e8b
Hoang Kiet <phamminhduc26031997@gmail.com>
2026-03-17 14:38:22 +0700

    update background

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,14 @@
 // console.log("Hello, world!"); 
+document.addEventListener("DOMContentLoaded", function() {
+  const vidIntro = document.getElementById('vid-intro');
+  const vidLoop = document.getElementById('vid-loop');
+
+  // Lắng nghe sự kiện 'ended' (kết thúc) của Video 1
+  vidIntro.addEventListener('ended', function() {
+    
+    vidLoop.style.opacity = "1"; // 1. Hiện Video 2 lên
+    vidLoop.play();              // 2. Cho Video 2 bắt đầu chạy
+    vidIntro.style.opacity = "0"; // 3. Giấu Video 1 đi 
+    
+  });
+});
