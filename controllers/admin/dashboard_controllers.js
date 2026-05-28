ea672e429927d5f74973c2cc5b1f37106f282f2b
Ngô Trường <truong@example.com>
2026-05-28 18:02:17 +0700

    fix file .env

--- controllers/admin/dashboard_controllers.js
+++ controllers/admin/dashboard_controllers.js
@@ -1,6 +1,29 @@
-// [GET] /admin/dashboard
-module.exports.dashboard = (req, res) => {
-  res.render('admin/pages/dashboard/index', {
-    pageTitle: "Trang tong quan"
-  })
-}
+const Product = require("../../model/product_model");
+const ProductCategory = require("../../model/product-category.model");
+const Order = require("../../model/order.model");
+const Account = require("../../model/account.model");
+
+module.exports.dashboard = async (req, res) => {
+  const totalProduct = await Product.countDocuments({ deleted: false });
+  const totalCategory = await ProductCategory.countDocuments({ deleted: false });
+  const totalOrder = await Order.countDocuments();
+  const totalAccount = await Account.countDocuments({ deleted: false });
+
+  const latestOrders = await Order.find({})
+    .sort({ createdAt: -1 })
+    .limit(5);
+
+  const latestProducts = await Product.find({ deleted: false })
+    .sort({ createdAt: -1 })
+    .limit(5);
+
+  res.render("admin/pages/dashboard/index", {
+    pageTitle: "Trang tổng quan",
+    totalProduct,
+    totalCategory,
+    totalOrder,
+    totalAccount,
+    latestOrders,
+    latestProducts
+  });
+};
